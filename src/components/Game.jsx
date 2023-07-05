86b816be16275895494adb26ea295e5a1e85be6d
Jack_work <jacquespariseau93@gmail.com>
2023-07-05 14:49:51 -0400

    added more to the upper section of the yahtzee scorecard

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,22 +1,52 @@
 
 const Game = ({playerList, isGameOver, setGameOver, setWinner}) => {
     return (
-        <section className="flex flex-col items-center">
+        <section id="game" className="flex flex-col items-center max-h-screen">
             <h1 className="font-bold text-5xl mb-5">Yahtzee!</h1>
-            <table className="table-auto text-center justify-center">
-                <thead>
-                    <tr>
-                        <th>Upper Section</th>
+            <table className="table-fixed text-center self-start w-full overflow-scroll border border-collapse">
+                <thead className="border">
+                    <tr className="border">
+                        <th className="p-2 border">Upper Section</th>
                         {playerList.map((name, index) => {
-                            return <th key={`${name}${index}`}>{name}</th>
+                            return <th className="p-2 border" key={`${name}${index}`}>{name}</th>
                         })}
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>ones</td>
+                        <td className="p-2 border">ones</td>
                         {playerList.map((name, index) => {
-                            return <td><input key={`${name}Ones`} type="number" className="text-center" /></td>
+                            return <td className="p-2 border" key={`${name}Ones`}><input type="number" max={6} className="text-center w-full h-full" /></td>
+                        })}
+                    </tr>
+                    <tr>
+                        <td className="p-2 border">twos</td>
+                        {playerList.map((name, index) => {
+                            return <td className="p-2 border" key={`${name}Twos`}><input type="number" max={12} className="text-center w-full h-full" /></td>
+                        })}
+                    </tr>
+                    <tr>
+                        <td className="p-2 border">threes</td>
+                        {playerList.map((name, index) => {
+                            return <td className="p-2 border" key={`${name}Threes`}><input type="number" max={18} className="text-center w-full h-full" /></td>
+                        })}
+                    </tr>
+                    <tr>
+                        <td className="p-2 border">fours</td>
+                        {playerList.map((name, index) => {
+                            return <td className="p-2 border" key={`${name}Fours`}><input type="number" max={24} className="text-center w-full h-full" /></td>
+                        })}
+                    </tr>
+                    <tr>
+                        <td className="p-2 border">fives</td>
+                        {playerList.map((name, index) => {
+                            return <td className="p-2 border" key={`${name}Fives`}><input type="number" max={30} className="text-center w-full h-full" /></td>
+                        })}
+                    </tr>
+                    <tr>
+                        <td className="p-2 border">sixes</td>
+                        {playerList.map((name, index) => {
+                            return <td className="p-2 border" key={`${name}Sixes`}><input type="number" max={36} className="text-center w-full h-full" /></td>
                         })}
                     </tr>
                 </tbody>
